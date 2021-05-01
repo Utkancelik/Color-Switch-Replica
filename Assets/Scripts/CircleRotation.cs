376f48e10f35fc0d7bc30dd1e4e96865e7457f94
Utkan Çelik <utkancelik@hacettepe.edu.tr>
2021-05-01 16:06:29 +0300

    Initial commit

---
+++ Assets/Scripts/CircleRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CircleRotation : MonoBehaviour
+{
+    public float rotationSpeed;
+    void Update()
+    {
+        transform.Rotate(0f,0f,rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject colorChanger;
+ 
+
+    private Rigidbody2D rb2D;
+    public float jumpForce = 10f;
+    private SpriteRenderer sr;
+
+    public string currentColor;
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorPink;
+    public Color colorMagenta;
+    void Start()
+    {
+        
+        rb2D = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        SetRandomColor();
+        
+    }
+
+    
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            //Jump here
+            rb2D.velocity = Vector2.up * jumpForce; 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != currentColor)
+        {
+            Debug.Log("GAME OVER!");
+        }
+
+        if (collision.tag == "colorChanger")
+        {
+            SetRandomColor();
+            Destroy(colorChanger);
+        }
+       
+    }
+
+    void SetRandomColor()
+    {
+        int randomInt = Random.Range(1, 5);
+
+        switch (randomInt)
+        {
+            case 1:
+                currentColor = "cyan";
+                sr.color = colorCyan;
+                break;
+            case 2:
+                currentColor = "yellow";
+                sr.color = colorYellow;
+                break;
+            case 3:
+                currentColor = "pink";
+                sr.color = colorPink;
+                break;
+            case 4:
+                currentColor = "magenta";
+                sr.color = colorMagenta;
+                break;
+            
+        }
+
+        
+
+
+    }
+}
